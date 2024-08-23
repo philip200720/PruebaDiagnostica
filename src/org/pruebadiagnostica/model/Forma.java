e57817403eab4bf5af0b0dc35e6144398b4a07e5
Informatica <informatica@alumno.local>
2024-08-23 16:14:17 -0600

    feat: Se modifica el metodo abstracto y se agrega getters y setters

--- src/org/pruebadiagnostica/model/Forma.java
+++ src/org/pruebadiagnostica/model/Forma.java
@@ -3,6 +3,13 @@ package org.pruebadiagnostica.model;
 public abstract class Forma {
     private String color;
 
-    public abstract void calcularArea(){
+    public abstract void calcularArea();
+
+    public String getColor() {
+        return color;
     }
+
+    public void setColor(String color) {
+        this.color = color;
+    }    
 }
